572d910f9c7806bd973a7d8f1086128fa23fb1d2
20020484 <84169231+20020484@users.noreply.github.com>
2021-11-28 15:57:32 +0700

    Add files via upload

---
+++ src/BombCounter.java
@@ -0,0 +1,18 @@
+public class BombCounter extends Item
+{
+
+    public BombCounter(int rowIndex, int colIndex) {
+        super(colIndex, rowIndex);
+    }
+
+    public void addToPlayer(Player player) {
+        int currentBombCount = player.getBombCount();
+        player.setBombCount(currentBombCount + 1);
+    }
+
+    public String getName() {
+        final String name = "BombCounter";
+        return name;
+    }
+}
+
---
+++ src/SpeedUp.java
@@ -0,0 +1,17 @@
+public class SpeedUp extends Item
+{
+
+    public SpeedUp(int rowIndex, int colIndex) {
+        super( colIndex, rowIndex);
+    }
+
+    public void addToPlayer(Player player) {
+        int speed = (int) player.getPLAYER_PIXELS_BY_STEP();
+        player.setPlayerPixelsByStep(speed+2);
+    }
+
+    public String getName() {
+        final String name = "SpeedUp";
+        return name;
+    }
+}
